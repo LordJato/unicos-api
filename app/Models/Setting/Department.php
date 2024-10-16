e2b9f1110aecbe343c0d78f1585d7e5bdd2ecb2c
Steto Javellana <javellanasteto99@gmail.com>
2024-10-16 17:37:12 +0800

    add department table and migration

---
+++ app/Models/Setting/Department.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Setting;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Department extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'company_id'
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
