c7500e100665304a7e8e4fbbf3451801beada57e
Steto Javellana <javellanasteto99@gmail.com>
2024-10-23 23:19:42 +0800

    preparing job requirements

--- app/Models/Recruitment/JobRequirement.php
+++ app/Models/Recruitment/JobRequirement.php
@@ -5,7 +5,7 @@ namespace App\Models\Recruitment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class JobRequirements extends Model
+class JobRequirement extends Model
 {
     use HasFactory;
 }
