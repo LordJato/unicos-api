352c2002590c02959dce469dd1896b9760bab6d9
Steto Javellana <javellanasteto99@gmail.com>
2024-11-07 23:22:23 +0800

    add gate in employee update  request

--- app/Http/Requests/HR/EmployeeUpdateRequest.php
+++ app/Http/Requests/HR/EmployeeUpdateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\HR;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Http\FormRequest;
 
 class EmployeeUpdateRequest extends FormRequest
@@ -11,7 +12,7 @@ class EmployeeUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Gate::allows('update-employee');
     }
 
     /**
