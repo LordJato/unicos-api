9c92f4cb81182ccd3ec649d7c9752e1c0296b1f3
Steto Javellana <javellanasteto99@gmail.com>
2024-12-07 23:27:28 +0800

    preparing department

--- app/Http/Controllers/Setting/DepartmentController.php
+++ app/Http/Controllers/Setting/DepartmentController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Setting;
 
-use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
 
 class DepartmentController extends Controller
 {
-    //
+    use ResponseTrait;
+    
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            $validated = $request->validated();
+
+            return $this->responseSuccess($validated, "Account fetched successfully");
+        } catch (Exception $e) {
+            return $this->responseError([], $e->getMessage(), $this->getStatusCode($exception->getCode()));
+        }
+    }
+
 }
