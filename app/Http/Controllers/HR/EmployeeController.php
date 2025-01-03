bc223f1840dd25f0b60c13f9765b4a77dafb2ffb
Steto Javellana <javellanasteto99@gmail.com>
2025-01-03 23:33:46 +0800

    init employee controller

--- app/Http/Controllers/HR/EmployeeController.php
+++ app/Http/Controllers/HR/EmployeeController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\HR;
 
 use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\HR\EmployeeRepository;
 use App\Http\Requests\HR\Employee\EmployeeIndexRequest;
+use App\Http\Requests\HR\Employee\EmployeeCreateRequest;
 
 class EmployeeController extends Controller
 {
@@ -19,6 +22,50 @@ class EmployeeController extends Controller
             $data = $this->employeeRepository->getAll($request->all());
 
             return $this->responseSuccess($data, "Employees fetched successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
+
+    public function store(EmployeeCreateRequest $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $create = $this->employeeRepository->create($validatedData);
+
+            return $this->responseSuccess($create, "Employee created successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request): JsonResponse
+    {
+        try {
+
+            $find = $this->employeeRepository->getByID($request->query('id'));
+
+            return $this->responseSuccess($find, "Employee find successfully");
+        } catch (Exception $e) {
+
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        try {
+
+            $update = $this->employeeRepository->update($request->query('id'), $request->all());
+
+            return $this->responseSuccess($update, "Employee updated successfully");
         } catch (Exception $e) {
             return parent::handleException($e);
         }
