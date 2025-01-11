be8bb4f4fde7e38c4bc73979fa59c20decaaeda8
Steto Javellana <javellanasteto99@gmail.com>
2025-01-11 23:40:39 +0800

    done observance controller

--- app/Http/Controllers/Setting/ObservanceController.php
+++ app/Http/Controllers/Setting/ObservanceController.php
@@ -2,10 +2,92 @@
 
 namespace App\Http\Controllers\Setting;
 
-use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Repositories\Setting\ObservanceRepository;
 
 class ObservanceController extends Controller
 {
-    //
+    public function __construct(
+        private readonly ObservanceRepository $observanceRepository,
+    ) {}
+
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $data = $this->observanceRepository->getAll($validatedData);
+
+            return $this->responseSuccess($data, "Observance fetched successfully");
+        } catch (Exception $e) {
+
+            return parent::handleException($e);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $create = $this->observanceRepository->create($validatedData);
+
+            return $this->responseSuccess($create, "Observance created successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request): JsonResponse
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $find = $this->observanceRepository->getByID($validatedData['id']);
+
+            return $this->responseSuccess($find, "Observance find successfully");
+        } catch (Exception $e) {
+
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $update = $this->observanceRepository->update($validatedData);
+
+            return $this->responseSuccess($update, "Observance updated successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $delete = $this->observanceRepository->softDelete($validatedData);
+
+            return $this->responseSuccess($delete, "Observance deleted successfully");
+        } catch (Exception $e) {
+
+            return parent::handleException($e);
+        }
+    }
 }
