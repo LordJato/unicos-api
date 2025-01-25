90085256ba4124c1fcbad678aa6408e1c53a2404
Steto Javellana <javellanasteto99@gmail.com>
2025-01-25 23:31:33 +0800

    add filalble

--- app/Models/Recruitment/OpportunityBenefit.php
+++ app/Models/Recruitment/OpportunityBenefit.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class OpportunityBenefit extends Model
 {
     use HasFactory;
+   
+    protected $fillable = [
+        'opportunity_id',
+        'description',
+    ];
 }
