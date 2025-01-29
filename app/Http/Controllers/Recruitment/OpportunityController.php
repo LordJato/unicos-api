7596f92df4a4d01003c75ffc47905faf4d8282ca
Steto Javellana <javellanasteto99@gmail.com>
2025-01-29 23:53:06 +0800

    add crud on repository

--- app/Http/Controllers/Recruitment/OpportunityController.php
+++ app/Http/Controllers/Recruitment/OpportunityController.php
@@ -2,11 +2,75 @@
 
 namespace App\Http\Controllers\Recruitment;
 
-use App\Http\Controllers\Controller;
-use App\Http\Repositories\Recruitment\OpportunityRepository;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Models\Recruitment\Opportunity;
+use App\Http\Requests\HR\Employee\EmployeeUpdateRequest;
+use App\Http\Repositories\Recruitment\OpportunityRepository;
 
 class OpportunityController extends Controller
 {
     public function __construct(private readonly OpportunityRepository $opportunityRepository) {}
+
+    public function index(Request $request)
+    {
+        try {
+
+            $validatedData = $request->validated();
+
+            $data = $this->opportunityRepository->getAll($validatedData);
+
+            return $this->responseSuccess($data, "Opporyunity fetched successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $create = $this->opportunityRepository->create($validatedData);
+
+            return $this->responseSuccess($create, "Opportunity created successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request): JsonResponse
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $find = $this->opportunityRepository->getByID($validatedData['id']);
+
+            return $this->responseSuccess($find, "Opportunity find successfully");
+        } catch (Exception $e) {
+
+            return parent::handleException($e);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $update = $this->opportunityRepository->update($validatedData);
+
+            return $this->responseSuccess($update, "Opportunity updated successfully");
+        } catch (Exception $e) {
+            return parent::handleException($e);
+        }
+    }
 }
