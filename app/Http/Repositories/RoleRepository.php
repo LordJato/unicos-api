b295c847e764c19371c829e75512db7e1ffcd191
Steto Javellana <javellanasteto99@gmail.com>
2025-02-12 23:40:51 +0800

    refactor role index

--- app/Http/Repositories/RoleRepository.php
+++ app/Http/Repositories/RoleRepository.php
@@ -19,23 +19,17 @@ class RoleRepository
 
     public function getAll(array $params): array
     {
-        $search = $params['search'] ?? null;
-        $offset = $params['offset'] ?? 0;
-        $limit = $params['limit'] ?? 10;
-        $orderBy = $params['orderBy'] ?? 'id';
-        $orderDesc =  ($params['orderDesc'] ?? false) ? 'desc' : 'asc';
-        $withPermission =  $params['withPermission'] ?? false;
+        $search = $params['search'];
 
-        $accounts = Role::when($withPermission, fn ($query) => $query->with('permissions'))
-            ->when($search, fn($query, $search) => $query->where('name', 'like', $search . '%'))
-            ->orderBy($orderBy, $orderDesc)
-            ->paginate($limit, ['*'], 'page', floor($offset / $limit) + 1);
+        $accounts = Role::when($search, fn($query, $search) => $query->where('name', 'like', $search . '%'))
+            ->orderBy($params['orderBy'], $params['orderDesc'])
+            ->paginate($params['limit'], ['*'], 'page', floor($params['offset'] / $params['limit']) + 1);
 
         return [
             'total' => $accounts->total(),
             'records' => $accounts->items(),
-            'offset' => $offset,
-            'limit' => $limit
+            'offset' =>  $params['offset'],
+            'limit' => $params['limit']
         ];
     }
 
