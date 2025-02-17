3eac99b57b4b39c3138ab1e2c46ef2bf390f01c6
Steto Javellana <javellanasteto99@gmail.com>
2025-02-17 23:21:23 +0800

    preparing for versioning

--- routes/api.php
+++ routes/api.php
@@ -5,48 +5,51 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('password.forgot');
-Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
-Route::post('refresh-token', [AuthController::class, 'newAccessToken'])->name('refresh.token');
+//VERSION 1 API
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:api')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:api');
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('password.forgot');
+    Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+    Route::post('refresh-token', [AuthController::class, 'newAccessToken'])->name('refresh.token');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
 
 
-    Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
-        Route::get('profile', [UserController::class, 'profile']);
+        Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+            Route::get('profile', [UserController::class, 'profile']);
+        });
+
+
+        //Links
+        require_once __DIR__ . "/api/v1/links.php";
+
+        //Users
+        require_once __DIR__ . "/api/v1/users.php";
+
+        //Accounts
+        require_once __DIR__ . "/api/v1/accounts.php";
+
+        //Companies
+        require_once __DIR__ . "/api/v1/companies.php";
+
+        //Roles
+        require_once __DIR__ . "/api/v1/roles.php";
+
+        //Permissions
+        require_once __DIR__ . "/api/v1/permissions.php";
+
+        //HR Modules
+        require_once __DIR__ . '/api/v1/hr/index.php';
+
+        //Recruitment Modules
+        require_once __DIR__ . '/api/v1/recruitment/index.php';
     });
-
-     //Links
-     require __DIR__ . "/api/links.php";
-
-    //Users
-    require __DIR__ . "/api/users.php";
-
-    //Accounts
-    require __DIR__ . "/api/accounts.php";
-
-    //Companies
-    require __DIR__ . "/api/companies.php";
-
-    //Roles
-    require __DIR__ . "/api/roles.php";
-
-    //Permissions
-    require __DIR__ . "/api/permissions.php";
-
-    //Departments
-    require __DIR__ . "/api/hr/departments.php";
-
-    //Employees
-    require __DIR__ . "/api/hr/employees.php";
-
-     //Opportunities
-     require __DIR__ . "/api/recruitment/opportunities.php";
 });
