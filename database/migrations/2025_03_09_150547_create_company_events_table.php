81d9c96855043a7a06878b2ea6c6927fbb0b6364
Steto Javellana <javellanasteto99@gmail.com>
2025-03-09 23:07:20 +0800

    preparing company holiday and event

--- database/migrations/2025_03_09_150547_create_company_events_table.php
+++ database/migrations/2025_03_09_150547_create_company_events_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('company_events', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('company_id');
             $table->string('title', 100);
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('company_events');
     }
 };
