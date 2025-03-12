7dacbca1d3e6fb473608c2c71b6d98df2832782e
Steto Javellana <javellanasteto99@gmail.com>
2025-03-12 23:00:24 +0800

    init company event request

---
+++ app/Http/Requests/v1/Setup/CompanyEvent/CompanyEventRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\v1\Setup\Department;
+
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\v1\ApiFormRequest;
+
+class DepartmentCreateRequest extends ApiFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('create-department');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:50', 'unique:departments'],
+        ];
+    }
+}
---
+++ app/Http/Requests/v1/Setup/CompanyEvent/CompanyEventUpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\v1\Setup\Department;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\v1\ApiFormRequest;
+
+class DepartmentUpdateRequest extends ApiFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('update-department');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:50', Rule::unique('departments')->ignore($this->id, 'id')],
+        ];
+    }
+}
