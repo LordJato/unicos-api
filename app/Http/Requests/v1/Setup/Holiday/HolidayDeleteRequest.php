923aca9b04e5fa51d356f976e696514128552555
Steto Javellana <javellanasteto99@gmail.com>
2025-03-12 22:59:04 +0800

    init holiday request

---
+++ app/Http/Requests/v1/Setup/Holiday/HolidayDeleteRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\v1\Setup\Department;
+
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\v1\ApiFormRequest;
+
+class DepartmentDeleteRequest extends ApiFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('delete-department');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
---
+++ app/Http/Requests/v1/Setup/Holiday/HolidayGetRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\v1\Setup\Department;
+
+use App\Http\Requests\v1\ApiFormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class DepartmentGetRequest extends ApiFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('view-department');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
