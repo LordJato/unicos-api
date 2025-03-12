e32d554647e350ef4657429f777bb2cef6337b61
Steto Javellana <javellanasteto99@gmail.com>
2025-03-12 23:43:03 +0800

    preparing holiday and company policy

---
+++ app/Policies/CompanyEventPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\CompanyEvent;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CompanyEventPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, CompanyEvent $companyEvent): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, CompanyEvent $companyEvent): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, CompanyEvent $companyEvent): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, CompanyEvent $companyEvent): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, CompanyEvent $companyEvent): bool
+    {
+        //
+    }
+}
