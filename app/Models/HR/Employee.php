fcf1a7991e68609879489b9f737c549cfb57475f
Steto Javellana <javellanasteto99@gmail.com>
2025-03-20 23:37:04 +0800

    add employee data

--- app/Models/HR/Employee.php
+++ app/Models/HR/Employee.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'account_id',
+        'empployee_type_id',
+        'empployee_status_id',
+        'designation_id',
+        'shift_id',
+        'id_no',
+        'firstname',
+        'lastname',
+        'middlename',
+        'date_hired',
+        'date_resigned',
+        'date_terminated',
+        'tin',
+        'sss',
+        'pagibig',
+        'philhealth',
+        'ecola',
+        'basic_salary',
+        'hourly_rate'
+    ];
 }
