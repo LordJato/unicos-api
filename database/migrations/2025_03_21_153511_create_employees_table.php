2ba2fa623e87139129d25c98f96766644302b263
Steto Javellana <javellanasteto99@gmail.com>
2025-03-21 23:47:12 +0800

    fix employee table

--- database/migrations/2025_03_21_153511_create_employees_table.php
+++ database/migrations/2025_03_21_153511_create_employees_table.php
@@ -13,7 +13,33 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('employee_type_id');
+            $table->unsignedBigInteger('employee_status_id');
+            $table->unsignedBigInteger('designation_id');
+            $table->unsignedBigInteger('shift_id');
+            $table->string('id_no', 20);
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('middlename', 50);
+            $table->date('date_hired');
+            $table->date('date_resigned');
+            $table->date('date_terminated');
+            $table->string('tin', 30);
+            $table->string('sss', 30);
+            $table->string('pagibig', 30);
+            $table->string('philhealth', 30);
+            $table->mediumInteger('ecola');
+            $table->mediumInteger('basic_salary');
+            $table->mediumInteger('hourly_rate');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('employee_type_id')->references('id')->on('employee_types')->onDelete('cascade');
+            $table->foreign('employee_status_id')->references('id')->on('employee_statuses')->onDelete('cascade');
+            $table->foreign('designation_id')->references('id')->on('designations')->onDelete('cascade');
+            $table->foreign('shift_header_id')->references('id')->on('shift_headers')->onDelete('cascade');
         });
     }
 
