ad331b76196dbd4b9f53bbb4a01c2017288645c1
Steto Javellana <javellanasteto99@gmail.com>
2025-03-21 23:54:10 +0800

    add nullable on description of designation

--- database/migrations/2025_03_16_155113_create_designations_table.php
+++ database/migrations/2025_03_16_155113_create_designations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('designations', function (Blueprint $table) {
             $table->id();
             $table->string('title', 50);
-            $table->string('description', 200);
+            $table->string('description', 200)->nullable();
             $table->string('slug', 100);
         });
     }
