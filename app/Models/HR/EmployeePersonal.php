93f96e0b8fe00c27607bd5aeef77ae9b7abd21a5
Steto Javellana <javellanasteto99@gmail.com>
2025-03-29 23:43:47 +0800

    preparing personal

---
+++ app/Models/HR/EmployeePersonal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\HR;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeePersonal extends Model
+{
+    use HasFactory;
+}
