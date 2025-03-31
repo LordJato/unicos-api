94978cfc38a6896f2a5ddc3966da8a959d24e005
Steto Javellana <javellanasteto99@gmail.com>
2025-03-31 23:39:41 +0800

    add fillable on employee personal mode

--- app/Models/HR/EmployeePersonal.php
+++ app/Models/HR/EmployeePersonal.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeePersonal extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'employee_id',
+        'civil_status_id',
+        'birth_date',
+        'birth_place',
+        'sex',
+        'citizenship',
+        'nationality',
+        'religion',
+        'phone',
+        'email',
+        'spouse',
+        'spouse_occupation',
+        'mother',
+        'mother_occupation',
+        'father',
+        'father_occupation',
+    ];
 }
