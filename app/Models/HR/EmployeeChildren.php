e16e2e0a2aa50cd5bcc7db25a3647f50e7502169
Steto Javellana <javellanasteto99@gmail.com>
2025-03-31 23:41:58 +0800

    add fillable on employee children  model

--- app/Models/HR/EmployeeChildren.php
+++ app/Models/HR/EmployeeChildren.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeeChildren extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'name',
+        'birth_date',
+        'birth_certificate',
+    ];
+    
 }
