2b0191ba1d9fb2f6726fce975632a351161deb8d
Steto Javellana <javellanasteto99@gmail.com>
2025-04-06 22:26:56 +0800

    fixed opportunity benefit

--- app/Http/Controllers/v1/Recruitment/OpportunityBenefitController.php
+++ app/Http/Controllers/v1/Recruitment/OpportunityBenefitController.php
@@ -2,33 +2,37 @@
 
 namespace App\Http\Controllers\v1\Recruitment;
 
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\v1\Controller;
+use App\Http\Requests\v1\Recruitment\OpportunityBenefit\OpportunityBenefitCreateRequest;
+use App\Http\Requests\v1\Recruitment\OpportunityBenefit\OpportunityBenefitIndexRequest;
+use App\Http\Requests\v1\Recruitment\OpportunityBenefit\OpportunityBenefitUpdateRequest;
 use App\Repositories\v1\Recruitment\OpportunityBenefitRepository;
-use Illuminate\Http\Request;
 
-class OpportunityBenefitController
+class OpportunityBenefitController extends Controller
 {
-    public function __construct(private readonly OpportunityBenefitRepository $opportunityBenefit) {}
+    public function __construct(private readonly OpportunityBenefitRepository $opportunityBenefitRepository) {}
 
-    public function index(Request $request)
+    public function index(OpportunityBenefitIndexRequest $request)
     {
-        $this->checkPermission('view-all-opportunity');
+        $this->checkPermission('view-all-opportunity-benefit');
 
         $validatedData = $request->validated();
 
-        $data = $this->opportunityRepository->getAll($validatedData);
+        $data = $this->opportunityBenefitRepository->getAll($validatedData);
 
-        return $this->responseSuccess($data, "Opporyunity fetched successfully");
+        return $this->responseSuccess($data, "Opporyunity Benefits fetched successfully");
     }
 
-    public function store(OpportunityCreateRequest $request)
+    public function store(OpportunityBenefitCreateRequest $request)
     {
-        $this->checkPermission('create-opportunity');
+        $this->checkPermission('create-opportunity-benefit');
 
         $validatedData = $request->validated();
 
-        $create = $this->opportunityRepository->create($validatedData);
+        $create = $this->opportunityBenefitRepository->create($validatedData);
 
-        return $this->responseSuccess($create, "Opportunity created successfully");
+        return $this->responseSuccess($create, "Opportunity Benefit created successfully");
     }
 
     /**
@@ -36,25 +40,25 @@ class OpportunityBenefitController
      */
     public function show($id): JsonResponse
     {
-        $this->checkPermission('view-opportunity');
+        $this->checkPermission('view-opportunity-benefit');
 
-        $find = $this->opportunityRepository->getByID($id);
+        $find = $this->opportunityBenefitRepository->getByID($id);
 
-        return $this->responseSuccess($find, "Opportunity find successfully");
+        return $this->responseSuccess($find, "Opportunity Benefit find successfully");
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update($id, OpportunityUpdateRequest $request)
+    public function update($id, OpportunityBenefitUpdateRequest $request)
     {
-        $this->checkPermission('update-opportunity');
+        $this->checkPermission('update-opportunity-benefit');
 
         $validatedData = $request->validated();
 
-        $update = $this->opportunityRepository->update($id, $validatedData);
+        $update = $this->opportunityBenefitRepository->update($id, $validatedData);
 
-        return $this->responseSuccess($update, "Opportunity updated successfully");
+        return $this->responseSuccess($update, "Opportunity Benefit updated successfully");
     }
 
     /**
@@ -62,10 +66,10 @@ class OpportunityBenefitController
      */
     public function destroy($id)
     {
-        $this->checkPermission('delete-opportunity');
+        $this->checkPermission('delete-opportunity-benefit');
 
-        $delete = $this->opportunityRepository->softDelete($id);
+        $delete = $this->opportunityBenefitRepository->softDelete($id);
 
-        return $this->responseSuccess($delete, "Opportunity deleted successfully");
+        return $this->responseSuccess($delete, "Opportunity Benefit deleted successfully");
     }
 }
