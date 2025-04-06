7d0c1dcaecccaf40287669032df0794b3601ba42
Steto Javellana <javellanasteto99@gmail.com>
2025-04-06 22:23:50 +0800

    add opportunity benefit request

--- app/Http/Controllers/v1/Recruitment/OpportunityBenefitController.php
+++ app/Http/Controllers/v1/Recruitment/OpportunityBenefitController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers\v1\Recruitment;
 
+use App\Repositories\v1\Recruitment\OpportunityBenefitRepository;
 use Illuminate\Http\Request;
 
 class OpportunityBenefitController
 {
-    //
+    public function __construct(private readonly OpportunityBenefitRepository $opportunityBenefit) {}
+
+    public function index(Request $request)
+    {
+        $this->checkPermission('view-all-opportunity');
+
+        $validatedData = $request->validated();
+
+        $data = $this->opportunityRepository->getAll($validatedData);
+
+        return $this->responseSuccess($data, "Opporyunity fetched successfully");
+    }
+
+    public function store(OpportunityCreateRequest $request)
+    {
+        $this->checkPermission('create-opportunity');
+
+        $validatedData = $request->validated();
+
+        $create = $this->opportunityRepository->create($validatedData);
+
+        return $this->responseSuccess($create, "Opportunity created successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id): JsonResponse
+    {
+        $this->checkPermission('view-opportunity');
+
+        $find = $this->opportunityRepository->getByID($id);
+
+        return $this->responseSuccess($find, "Opportunity find successfully");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update($id, OpportunityUpdateRequest $request)
+    {
+        $this->checkPermission('update-opportunity');
+
+        $validatedData = $request->validated();
+
+        $update = $this->opportunityRepository->update($id, $validatedData);
+
+        return $this->responseSuccess($update, "Opportunity updated successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $this->checkPermission('delete-opportunity');
+
+        $delete = $this->opportunityRepository->softDelete($id);
+
+        return $this->responseSuccess($delete, "Opportunity deleted successfully");
+    }
 }
