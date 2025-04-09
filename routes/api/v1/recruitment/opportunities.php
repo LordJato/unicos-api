8f78fce36c9bf4d055d2848de73209dacad9852a
Steto Javellana <javellanasteto99@gmail.com>
2025-04-09 22:27:10 +0800

    fixed api routes for opportunity

--- routes/api/v1/recruitment/opportunities.php
+++ routes/api/v1/recruitment/opportunities.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\v1\Recruitment\Opportunity\BenefitController;
+use App\Http\Controllers\v1\Recruitment\Opportunity\RequirementController;
+use App\Http\Controllers\v1\Recruitment\Opportunity\ResponsibilityController;
 use App\Http\Controllers\v1\Recruitment\OpportunityController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::apiResource('opportunities', OpportunityController::class);
+
+Route::prefix('opportunities')->group(function () {
+    
+    Route::prefix('{opportunity}')->group(function () {
+
+        Route::apiResource('benefits', BenefitController::class);
+        
+        Route::apiResource('requirements', RequirementController::class);
+
+        Route::apiResource('resonsibilites', ResponsibilityController::class);
+    });
+});
