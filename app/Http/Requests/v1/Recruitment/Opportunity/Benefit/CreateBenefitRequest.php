e4da2e3d9aee042e8429691fb89a9297cf5d4065
Steto Javellana <javellanasteto99@gmail.com>
2025-04-15 22:52:44 +0800

    fixed create benefit request

--- app/Http/Requests/v1/Recruitment/Opportunity/Benefit/CreateBenefitRequest.php
+++ app/Http/Requests/v1/Recruitment/Opportunity/Benefit/CreateBenefitRequest.php
@@ -11,7 +11,7 @@ class CreateBenefitRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class CreateBenefitRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'opportunity_id' => [
+                'required',
+                'integer',
+                'exists:opportunities,id',
+            ],
+            'description' => [
+                'required',
+                'string',
+                'max:255',
+            ],
         ];
     }
 }
