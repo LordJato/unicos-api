3fa767446e5e94bd7cd4e30a69c6d792abd68b16
Steto Javellana <javellanasteto99@gmail.com>
2025-04-15 22:54:34 +0800

    fixed create requirement fornm request

--- app/Http/Requests/v1/Recruitment/Opportunity/Requirement/CreateRequirementRequest.php
+++ app/Http/Requests/v1/Recruitment/Opportunity/Requirement/CreateRequirementRequest.php
@@ -11,7 +11,7 @@ class CreateRequirementRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class CreateRequirementRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'opportunity_id' => [
+                'required',
+                'integer',
+                'exists:opportunities,id',
+            ],
+            'description' => [
+                'required',
+                'string',
+                'max:255',
+            ],
         ];
     }
 }
