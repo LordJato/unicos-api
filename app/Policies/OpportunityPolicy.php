8f72ccec6174624be5a5fc5e24707aec3fb2178a
Steto Javellana <javellanasteto99@gmail.com>
2025-04-16 22:49:22 +0800

    add policy

--- app/Policies/OpportunityPolicy.php
+++ app/Policies/OpportunityPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Holiday;
+use App\Models\Opportunity;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class HolidayPolicy
+class OpportunityPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class HolidayPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Holiday $holiday): bool
+    public function view(User $user, Opportunity $opportunity): bool
     {
         return true;
     }
@@ -35,7 +35,7 @@ class HolidayPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Holiday $holiday): bool
+    public function update(User $user, Opportunity $opportunity): bool
     {
         return true;
     }
@@ -43,7 +43,7 @@ class HolidayPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Holiday $holiday): bool
+    public function delete(User $user, Opportunity $opportunity): bool
     {
         return true;
     }
@@ -51,7 +51,7 @@ class HolidayPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Holiday $holiday): bool
+    public function restore(User $user, Opportunity $opportunity): bool
     {
         return true;
     }
@@ -59,7 +59,7 @@ class HolidayPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Holiday $holiday): bool
+    public function forceDelete(User $user, Opportunity $opportunity): bool
     {
         return true;
     }
