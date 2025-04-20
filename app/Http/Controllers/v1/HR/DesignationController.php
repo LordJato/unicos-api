adfc4c171547679ee054182470c135a76ebe2d82
Steto Javellana <javellanasteto99@gmail.com>
2025-04-20 22:49:25 +0800

    add designation contoller repository

--- app/Http/Controllers/v1/HR/DesignationController.php
+++ app/Http/Controllers/v1/HR/DesignationController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Repositories\v1\HR\DesignationRepository;
 use Illuminate\Http\Request;
 
 class DesignationController
 {
-    //
+    public function __construct(private readonly DesignationRepository $designationRepository) {}
 }
