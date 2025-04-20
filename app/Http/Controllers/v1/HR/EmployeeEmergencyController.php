6bd00f037243269fa0a4a71a23434f3e6543d1b8
Steto Javellana <javellanasteto99@gmail.com>
2025-04-20 22:51:04 +0800

    inject repository to employee emergency contoller

--- app/Http/Controllers/v1/HR/EmployeeEmergencyController.php
+++ app/Http/Controllers/v1/HR/EmployeeEmergencyController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Repositories\v1\HR\Employee\EmployeeEmergencyRepository;
 use Illuminate\Http\Request;
 
 class EmployeeEmergencyController
 {
-    //
+    public function __construct(private readonly EmployeeEmergencyRepository $employeeEmergencyRepository) {}
 }
