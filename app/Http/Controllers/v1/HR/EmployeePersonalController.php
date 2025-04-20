7f56a1e67441d1493d43cf34f133c3c91e65fce5
Steto Javellana <javellanasteto99@gmail.com>
2025-04-20 22:51:31 +0800

    inject repository to employee personal contoller

--- app/Http/Controllers/v1/HR/EmployeePersonalController.php
+++ app/Http/Controllers/v1/HR/EmployeePersonalController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Repositories\v1\HR\Employee\EmployeePersonalRepository;
 use Illuminate\Http\Request;
 
 class EmployeePersonalController
 {
-    //
+    public function __construct(private readonly EmployeePersonalRepository $employeePersonalRepository) {}
 }
