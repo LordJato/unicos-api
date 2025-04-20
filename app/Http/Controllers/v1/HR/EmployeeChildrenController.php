b65fcccea596dd7b54b26a349cc8c5e141c7a48a
Steto Javellana <javellanasteto99@gmail.com>
2025-04-20 22:50:29 +0800

    inject repository to employee children  contoller

--- app/Http/Controllers/v1/HR/EmployeeChildrenController.php
+++ app/Http/Controllers/v1/HR/EmployeeChildrenController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Repositories\v1\HR\Employee\EmployeeChildrenRepository;
 use Illuminate\Http\Request;
 
 class EmployeeChildrenController
 {
-    //
+    public function __construct(private readonly EmployeeChildrenRepository $employeeChildrenRepository) {}
 }
