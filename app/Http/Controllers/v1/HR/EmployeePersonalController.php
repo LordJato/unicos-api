d6c6c52033d9c1f2503bb2bbf91565f55ce957b6
Steto Javellana <javellanasteto99@gmail.com>
2025-04-21 21:10:53 +0800

    add fetchAll in employee personal

--- app/Http/Controllers/v1/HR/EmployeePersonalController.php
+++ app/Http/Controllers/v1/HR/EmployeePersonalController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Http\Controllers\v1\Controller;
 use App\Repositories\v1\HR\Employee\EmployeePersonalRepository;
 use Illuminate\Http\Request;
 
-class EmployeePersonalController
+class EmployeePersonalController extends Controller
 {
     public function __construct(private readonly EmployeePersonalRepository $employeePersonalRepository) {}
+
+    public function index(Request $request)
+    {
+        $validatedData = $request->validated();
+
+        $data = $this->employeePersonalRepository->getAll($validatedData);
+
+        return $this->responseSuccess($data, "Employees fetched successfully");
+    }
+
 }
