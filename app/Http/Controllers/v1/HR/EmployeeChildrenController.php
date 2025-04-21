52087951aa4c6ee20fee9b5607990dbf86ff95bd
Steto Javellana <javellanasteto99@gmail.com>
2025-04-21 21:09:05 +0800

    add fetchAll in employee children

--- app/Http/Controllers/v1/HR/EmployeeChildrenController.php
+++ app/Http/Controllers/v1/HR/EmployeeChildrenController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Http\Controllers\v1\Controller;
 use App\Repositories\v1\HR\Employee\EmployeeChildrenRepository;
 use Illuminate\Http\Request;
 
-class EmployeeChildrenController
+class EmployeeChildrenController extends Controller
 {
     public function __construct(private readonly EmployeeChildrenRepository $employeeChildrenRepository) {}
+
+    public function index(Request $request)
+    {
+        $validatedData = $request->validated();
+
+        $data = $this->employeeChildrenRepository->getAll($validatedData);
+
+        return $this->responseSuccess($data, "Employees fetched successfully");
+    }
+
 }
