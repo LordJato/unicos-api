95bb5ce5609a70fd38f2f30e57dde43f7eba532f
Steto Javellana <javellanasteto99@gmail.com>
2025-04-21 21:10:12 +0800

    add fetchAll in employee emergency

--- app/Http/Controllers/v1/HR/EmployeeEmergencyController.php
+++ app/Http/Controllers/v1/HR/EmployeeEmergencyController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Http\Controllers\v1\Controller;
 use App\Repositories\v1\HR\Employee\EmployeeEmergencyRepository;
 use Illuminate\Http\Request;
 
-class EmployeeEmergencyController
+class EmployeeEmergencyController extends Controller
 {
     public function __construct(private readonly EmployeeEmergencyRepository $employeeEmergencyRepository) {}
+
+    public function index(Request $request)
+    {
+        $validatedData = $request->validated();
+
+        $data = $this->employeeEmergencyRepository->getAll($validatedData);
+
+        return $this->responseSuccess($data, "Employees fetched successfully");
+    }
 }
