7583f20da356ba72965c90831f388ecce56b73da
Steto Javellana <javellanasteto99@gmail.com>
2025-04-21 21:11:30 +0800

    add fetchAll in designation

--- app/Http/Controllers/v1/HR/DesignationController.php
+++ app/Http/Controllers/v1/HR/DesignationController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\v1\HR;
 
+use App\Http\Controllers\v1\Controller;
 use App\Repositories\v1\HR\DesignationRepository;
 use Illuminate\Http\Request;
 
-class DesignationController
+class DesignationController extends Controller
 {
     public function __construct(private readonly DesignationRepository $designationRepository) {}
+
+    public function index(Request $request)
+    {
+        $validatedData = $request->validated();
+
+        $data = $this->designationRepository->getAll($validatedData);
+
+        return $this->responseSuccess($data, "Employees fetched successfully");
+    }
 }
