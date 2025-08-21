59e96b740c913e98c3389edb154a2db260e01899
Steto Javellana <javellanasteto99@gmail.com>
2025-08-21 23:11:21 +0800

    Update AccountPolicy.php

--- app/Policies/AccountPolicy.php
+++ app/Policies/AccountPolicy.php
@@ -24,41 +24,26 @@ class AccountPolicy
         return $user->account_id === $account->id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, Account $account): bool
     {
         return $user->account_id === $account->id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, Account $account): bool
     {
         return $user->account_id === $account->id;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
     public function restore(User $user, Account $account): bool
     {
         return false;
     }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
+    
     public function forceDelete(User $user, Account $account): bool
     {
         return false;
