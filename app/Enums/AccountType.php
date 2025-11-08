39e43e035a70a6881558e8875d4f6929f676e8ad
Steto Javellana <javellanasteto99@gmail.com>
2025-11-08 23:51:33 +0800

    Update AccountType.php

--- app/Enums/AccountType.php
+++ app/Enums/AccountType.php
@@ -5,20 +5,20 @@ namespace App\Enums;
 enum AccountType: int
 {
 
-    case Tenant = 1;
-    case Company = 2;
-    case Client = 3;
-    case Employee = 4;
-    case Freelancer = 5;
+    case TENANT = 1;
+    case COMPANY = 2;
+    case CLIENT = 3;
+    case EMPLOYEE = 4;
+    case QUESTOR = 5;
 
     public function label(): string
     {
         return match ($this) {
-            self::Tenant => 'Tenant',
-            self::Company => 'Company',
-            self::Client => 'Client',
-            self::Employee => 'Employee',
-            self::Freelancer => 'Freelancer',
+            self::TENANT => 'Tenant',
+            self::COMPANY => 'Company',
+            self::CLIENT => 'Client',
+            self::EMPLOYEE => 'Employee',
+            self::QUESTOR => 'Questor',
         };
     }
 }
